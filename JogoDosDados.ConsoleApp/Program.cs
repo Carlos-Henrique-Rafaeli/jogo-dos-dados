e0f5cf56aa7af0bb605e16aea8907b6b745f98d8
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-19 14:25:52 -0300

    Implementada lógica de avanço de posições do jogador

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -4,21 +4,44 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        const int limiteLinhaChegada = 30;
+
         while (true)
         {
-            Console.Clear();
-            Console.WriteLine("---------------------------------");
-            Console.WriteLine("Jogo dos Dados");
-            Console.WriteLine("---------------------------------");
+            int posicaoJogador = 0;
+            bool jogoEmAndamento = true;
 
-            Console.Write("\nPressione ENTER para lançar o dado...");
-            Console.ReadLine();
+            while (jogoEmAndamento)
+            {
+                Console.Clear();
+                Console.WriteLine("------------------------------------------------");
+                Console.WriteLine("Jogo dos Dados");
+                Console.WriteLine("------------------------------------------------");
 
-            Random geradorDeNumeros = new Random();
 
-            int resultado = geradorDeNumeros.Next(1, 7);
+                Console.Write("\nPressione ENTER para lançar o dado...");
+                Console.ReadLine();
 
-            Console.WriteLine($"\nO valor sorteado foi {resultado}!");
+                Random geradorDeNumeros = new Random();
+
+                int resultado = geradorDeNumeros.Next(1, 7);
+
+                Console.WriteLine($"\nO valor sorteado foi {resultado}!\n");
+
+                posicaoJogador += resultado;
+
+                if (posicaoJogador >= limiteLinhaChegada)
+                {
+                    jogoEmAndamento = false;
+                    Console.WriteLine("------------------------------------------------");
+                    Console.WriteLine("Parabéns, você alcançou a linha de chegada!");
+                    Console.WriteLine("------------------------------------------------");
+                }
+                else
+                    Console.WriteLine($"Você está na posição {posicaoJogador} de {limiteLinhaChegada}!");
+
+                Console.ReadLine();
+            }
 
             Console.Write("\nDeseja continuar? (S/N) ");
             string opcaoContinuar = Console.ReadLine()!.ToUpper();
