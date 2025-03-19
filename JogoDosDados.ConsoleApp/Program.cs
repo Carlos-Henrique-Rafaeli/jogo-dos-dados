b397d196c1b492daafc409954d0edae7f2b35a3b
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-19 14:12:00 -0300

    Implementada lógica de lançamento de dados

--- JogoDosDados.ConsoleApp/Program.cs
+++ JogoDosDados.ConsoleApp/Program.cs
@@ -4,6 +4,27 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Jogo dos Dados");
+            Console.WriteLine("---------------------------------");
+
+            Console.Write("\nPressione ENTER para lançar o dado...");
+            Console.ReadLine();
+
+            Random geradorDeNumeros = new Random();
+
+            int resultado = geradorDeNumeros.Next(1, 7);
+
+            Console.WriteLine($"\nO valor sorteado foi {resultado}!");
+
+            Console.Write("\nDeseja continuar? (S/N) ");
+            string opcaoContinuar = Console.ReadLine()!.ToUpper();
+
+            if (opcaoContinuar != "S")
+                break;
+        }
     }
 }
